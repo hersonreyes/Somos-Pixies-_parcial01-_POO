51e6a833d7ccc886a079f6c10929effe5ab65e7a
MJZF00155619 <00155619@uca.edu.sv>
2020-04-17 14:36:49 -0600

    Add Documento Class

---
+++ src/com/HARR/x00216519/Documento.java
@@ -0,0 +1,19 @@
+package com.HARR.x00216519;
+
+public class Documento {
+    private String nombre;
+    private String numero;
+
+    public Documento(String nombre, String numero) {
+        this.nombre = nombre;
+        this.numero = numero;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+}
