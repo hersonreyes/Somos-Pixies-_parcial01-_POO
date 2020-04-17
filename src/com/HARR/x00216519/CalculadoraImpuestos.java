cb1617794e3c5dc0321986a0c88efcf0db42d66f
MJZF00155619 <00155619@uca.edu.sv>
2020-04-17 15:19:25 -0600

    Add: CalculadoraImpuestos Class

---
+++ src/com/HARR/x00216519/CalculadoraImpuestos.java
@@ -0,0 +1,22 @@
+package com.HARR.x00216519;
+
+public final class CalculadoraImpuestos {
+    private double totalRenta;
+    private double totalISSS;
+    private double totalAFP;
+
+    public CalculadoraImpuestos(double totalRenta, double totalISSS, double totalAFP) {
+        this.totalRenta = totalRenta;
+        this.totalISSS = totalISSS;
+        this.totalAFP = totalAFP;
+    }
+
+    /*public double calcularPago(){
+
+    }*/
+
+    /*public String mostrarTotales(){
+
+    }*/
+
+}
