aa7d8649f4ad96c82f1f51ce3cf41c46671823e3
hersonreyes <54414689+hersonreyes@users.noreply.github.com>
2020-04-17 15:07:00 -0600

    Add: Class Empleado

---
+++ src/com/HARR/x00216519/Empleado.java
@@ -0,0 +1,61 @@
+package com.HARR.x00216519;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+abstract class Empleado {
+    protected String nombre;
+    protected String puesto;
+    protected ArrayList<Documento> doc;
+    protected double salario;
+
+    public Empleado(String nombre, String puesto, double salario) {
+        this.nombre = nombre;
+        this.puesto = puesto;
+        this.salario = salario;
+        ArrayList<Documento> doc=new ArrayList<Documento>();
+    }
+    Scanner scan=new Scanner(System.in);
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getPuesto() {
+        return puesto;
+    }
+
+    public ArrayList<Documento> getDoc() {
+        return doc;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public void addDocumento(){
+        System.out.println("Ingrese nombre del documento");
+        String nombre=scan.nextLine();
+        System.out.println("Ingrese numero del documento");
+        String numero=scan.nextLine();
+        doc.add(new Documento(nombre,numero));
+    }
+
+    public void removeDocument(String numero){
+        System.out.println("Ingrese el numero de documento a eliminar");
+            Documento aux = null;
+
+            for(Documento empleado : doc){
+                if(empleado.getNumero().equalsIgnoreCase(numero))
+                    aux = empleado;
+            }
+            if(aux != null) {
+                doc.remove(aux);
+            }
+        }
+    }
+
