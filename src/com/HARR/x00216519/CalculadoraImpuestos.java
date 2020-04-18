ec5b42dda0319d53aa1765ec6b668d4b8b8fb5fe
MJZF00155619 <00155619@uca.edu.sv>
2020-04-17 18:33:21 -0600

    Modify: CalculadoraImpuestos Class

--- src/com/HARR/x00216519/CalculadoraImpuestos.java
+++ src/com/HARR/x00216519/CalculadoraImpuestos.java
@@ -1,22 +1,57 @@
 package com.HARR.x00216519;
 
 public final class CalculadoraImpuestos {
-    private double totalRenta;
-    private double totalISSS;
-    private double totalAFP;
+    private double totalRenta = 0;
+    private double totalISSS = 0;
+    private double totalAFP = 0;
 
-    public CalculadoraImpuestos(double totalRenta, double totalISSS, double totalAFP) {
+    private CalculadoraImpuestos(double totalRenta, double totalISSS, double totalAFP) {
         this.totalRenta = totalRenta;
         this.totalISSS = totalISSS;
         this.totalAFP = totalAFP;
     }
 
-    /*public double calcularPago(){
+    public double calcularPago(Empleado empleado) {
+        double restante = empleado.getSalario();
+        if (empleado instanceof PlazaFija) {
+            totalAFP = 0.0625 * empleado.getSalario();
+            totalISSS = 0.03 * empleado.getSalario();
+            restante = empleado.getSalario() - totalAFP - totalISSS;
+            if (restante >= 0.01 && restante < 472) {
+                totalRenta = 0;
+            } else if (restante >= 472 && restante < 895.24) {
+                totalRenta = 0.1 * (restante - 472) + 17.75;
+            } else if (restante >= 895.24 && restante < 2038.10) {
+                totalRenta = 0.2 * (restante - 895.24) + 60;
+            } else if (restante >= 2038.10) {
+                totalRenta = 0.2 * (restante - 2038.10) + 288.57;
+            }
+        }else if (empleado instanceof ServicioProfesional){
+            totalRenta = 0.1*empleado.getSalario();
+        }
+            return restante-totalRenta;
+        }
 
-    }*/
+    public String mostrarTotales(Empleado empleado){
+        double restante = empleado.getSalario();
+        if (empleado instanceof PlazaFija) {
+            totalAFP = 0.0625 * empleado.getSalario();
+            totalISSS = 0.03 * empleado.getSalario();
+            restante = empleado.getSalario() - totalAFP - totalISSS;
+            if (restante >= 0.01 && restante < 472) {
+                totalRenta = 0;
+            } else if (restante >= 472 && restante < 895.24) {
+                totalRenta = 0.1 * (restante - 472) + 17.75;
+            } else if (restante >= 895.24 && restante < 2038.10) {
+                totalRenta = 0.2 * (restante - 895.24) + 60;
+            } else if (restante >= 2038.10) {
+                totalRenta = 0.2 * (restante - 2038.10) + 288.57;
+            }
+        }else if (empleado instanceof ServicioProfesional){
+            totalRenta = 0.1*empleado.getSalario();
+        }
+        return "Total ISSS: $"+totalISSS+"\nTotal Renta: $"+totalRenta+"\nTotal AFP: $"+totalAFP+"\nTotal de descuento realizado: "+ (totalISSS+totalRenta+totalAFP);
 
-    /*public String mostrarTotales(){
+    }
 
-    }*/
-
-}
+    }
